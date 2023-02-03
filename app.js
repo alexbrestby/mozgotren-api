6869af869a88daae145dec3af45efd133f27639b
Alex Leoniuk <alex.brest.by@gmail.com>
2023-02-03 16:40:54 +0300

    feat: CORS headers add

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const PORT = process.env.PORT ?? 5000
 const app = express()
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next()
 })
 app.use(express.json())
