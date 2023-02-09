06439a36e02b2975e308652f655526166ad2bac6
Alex Leoniuk <alex.brest.by@gmail.com>
2023-02-09 13:19:07 +0300

    fix: user check

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -30,24 +30,35 @@ class UserController {
     try {
       const errors = validationResult(req)
       if (!errors.isEmpty()) {
-        return res.status(400).json({ message: "Ошибка при регистрации", errors })
+        return res.status(400).json({
+          message: "Ошибка при регистрации",
+          errors,
+        });
       }
       const { username, password, email } = req.body;
-      const candidate = await User.findOne({ username })
+      const candidate = await User.findOne({ username });
       if (candidate) {
-        return res.status(400).json({ message: "Пользователь с таким именем уже существует" })
+        return res.status(400).json({
+          message: "Пользователь с таким именем уже существует"
+        });
       }
       const candidateMail = await User.findOne({ email })
       if (candidateMail) {
-        return res.status(400).json({ message: "Пользователь с таким email уже существует" })
+        return res.status(400).json({
+          message: "Пользователь с таким email уже существует"
+        });
       }
       const hashPassword = bcrypt.hashSync(password, 7);
       const user = new User({ username, password: hashPassword, email: email })
-      await user.save()
-      return res.json({ message: "Пользователь успешно зарегистрирован" })
+      await user.save();
+      return res.json({
+        message: "Пользователь успешно зарегистрирован"
+      });
     } catch (e) {
       console.log(e)
-      res.status(400).json({ message: 'Registration error' })
+      res.status(400).json({
+        message: 'Registration error'
+      });
     }
   }
 
@@ -56,16 +67,26 @@ class UserController {
       const { password, email } = req.body
       const user = await User.findOne({ email })
       if (!user) {
-        return res.status(400).json({ message: `Пользователь с email <b>${email}</b> не найден` })
+        return res.status(400).json({
+          message: `Пользователь с email <b>${email}</b> не найден`
+        });
       }
       const validPassword = bcrypt.compareSync(password, user.password)
       if (!validPassword) {
-        return res.status(400).json({ message: `Введен неверный пароль` })
+        return res.status(400).json({
+          message: `Введен неверный пароль`
+        });
       }
-      return res.json({ messsage: 'Вы вошли в систему', user: user.username, ssid: user._id });
+      return res.json({
+        messsage: 'Вы вошли в систему',
+        user: user.username,
+        ssid: user._id
+      });
     } catch (e) {
-      console.log(e)
-      res.status(400).json({ message: 'Login error' })
+      console.log(e);
+      res.status(400).json({
+        message: 'Login error'
+      });
     }
   }
 
@@ -73,6 +94,13 @@ class UserController {
     try {
       const { ssid } = req.body;
       const ssidDB = await User.findById(ssid);
+      if (!ssidDB) {
+        return res.json({
+          message: 'You are not autenticated',
+          status: false,
+          user: null,
+        });
+      }
       if (ssid === ssidDB.id) {
         return res.json(
           {
@@ -81,11 +109,17 @@ class UserController {
             user: ssidDB.username,
           });
       } else {
-        return res.json({ message: 'You are not authenticated', status: false, user: '' });
+        return res.json({
+          message: 'You are not authenticated',
+          status: false,
+          user: null,
+        });
       }
     } catch (e) {
       console.log(e)
-      res.status(400).json({ message: 'Check registration error' })
+      res.status(400).json({
+        message: 'Check registration error',
+      });
     }
   }
 }
